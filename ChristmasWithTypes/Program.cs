c8c2c767367e4c07f78434fa6fff13be575ed3ac
Dustin Key <Keydustin90@gmail.com>
2022-02-22 11:16:45 -0600

    comleted tree

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = null;
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{ "Xbox" , "credit for audible" , "office set up"}; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = null;
